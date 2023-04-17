002878599f48c5cbcd132081944b2521a269adb3
Sireethon Bunchongpru <werrty65@gmail.com>
2023-04-17 12:38:43 +0700

    Remove router

--- src/App.js
+++ src/App.js
@@ -32,25 +32,14 @@ function App() {
             <Header  />
             <div className='container'>
                 
-                
-                <Routes>
-                    <Route 
-                    exact 
-                    path='/' 
-                    element={
-                        <>
+            
                             <FeedbackForm handleAdd={addFeedback}/>
                             <FeedbackStats feedback={feedback}/>
                             <FeedbackList feedback={feedback} 
                             handleDelete={deleteFeedback}/>
-                        
-                        </>
-                    }
-                    ></Route>
-                    <Route path='/about' element={<AboutPage/>} />
-                </Routes>
-                <AboutIconLink />
+                    
             </div>
+            
         </Router>
         
     )
